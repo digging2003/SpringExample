adb8e837178ad2a2f0c0da9c82b210ae26f84f68
digging <digging@gmail>
2025-02-12 21:49:35 +0900

    [이지원] - mybatis 예제중

---
+++ src/main/java/com/digging/spring/ex/mybatis/service/ReviewService.java
@@ -0,0 +1,21 @@
+package com.digging.spring.ex.mybatis.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.digging.spring.ex.mybatis.domain.Review;
+import com.digging.spring.ex.mybatis.repository.ReviewRepository;
+
+@Service
+public class ReviewService {
+
+	@Autowired
+	private ReviewRepository reviewRepository;
+	// id가 5인 리뷰정보 얻어오기
+	public Review getReview() {
+		// new_review 테이블에서 id가 5인 행 조회
+		Review review = reviewRepository.selectReview();
+		return review;
+	}
+	
+}
